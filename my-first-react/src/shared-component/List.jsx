23dcecc91653cd76d05d590219ee85c093856e7a
muhmmdfath <fatihadli709@gmail.com>
2025-02-20 10:08:47 +0700

    add ci4

--- my-first-react/src/shared-component/List.jsx
+++ my-first-react/src/shared-component/List.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function List(props){
-  return(
-    <ul>
-      {
-        props.data.map(item => (
-          <React.Fragment>
-            <li>
-              Nama: {item.name}, Usia: {item.age}{" "}
-            </li>
-          </React.Fragment>
-        ))
-      }
-    </ul>
-  )
+// export default function List(props){
+//   return(
+//     <ul>
+//       {
+//         props.data.map(item => (
+//           <React.Fragment>
+//             <li>
+//               Nama: {item.name}, Usia: {item.age}{" "}
+//             </li>
+//           </React.Fragment>
+//         ))
+//       }
+//     </ul>
+//   )
+// }
+
+export default class List extends Component {
+  render(){
+    const {data} = this.props;
+    return(
+      <ul>
+        {
+          data.map(item => (
+            <React.Fragment>
+              <li>
+                Nama: {item.name}, Usia: {item.age}{" "}
+              </li>
+            </React.Fragment>
+          ))
+        }
+      </ul>
+    )
+  }
 }
